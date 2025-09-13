a67577cd1eb3746a67105d4252c7f4e62264943d
LEE LUNA <seojilee@student.42seoul.kr>
2025-09-13 17:06:22 +0900

    2nd week - 과제04: 몬테카를로 시뮬레이션으로 원주율과 타원의 면적 추정하기

--- 2nd-week/assignment04_LEESEOJIN_202500062.m
+++ 2nd-week/assignment04_LEESEOJIN_202500062.m
@@ -1,7 +1,7 @@
 % YSL.ACL.ESE.INU
 close all; clear all;
 
-k=100;
+k=10000;
 M=2000;
 m=0;
 pp=zeros(1,M); % zeros(sz1, ..., szM): 1*M 크기의 영행렬 반환.
@@ -13,18 +13,7 @@ for N = k:k:k*M % for N = start:step:end -> N=k, 2k, 3k, ..., (M-1)k, Mk
     y = -1 + 2 * rand(1, N); % -1 <= y <= 1, -1 <= x <= 1이어야 한다.
     z = sqrt(x.^2 + y.^2); % z(i) = x(i)^2 + y(i)^2 to every i.
 
-%    c = 0;
-%    d = 0;
-
-%    for i = 1 : N % 방금 생성한 좌표들이 단위원 내부에 있는지 확인.
-%        if z(i)<=1 % (x, y)가 단위원 내부에 있다면 (x^2 + y^2 = z^2 <= 1)
-%            c = c + 1; % count 늘리기.
-%        else
-%            d = d + 1; % 단위원 바깥의 점. c + d = 1*1 정사각형 내부의 점.
-%        end
-%    end
-
-% 너무 오래 걸려서 위 주석 처리된 코드를 아래 코드로 조금 바꿨습니다. 제 노트북이 불타려고 했습니다.
+% 너무 오래 걸려서 코드를 조금 바꿨습니다. 제 노트북이 불타려고 했습니다.
     c = sum(z <= 1); % 단위원 내부에 있는 점들을 센다. (sum: 조건식을 주면 조건에 해당하는 값들의 개수를 센다.)
     d = N - c;
 
@@ -36,7 +25,7 @@ end
 figure(1);clf; % 1번 창 열고 기존 그림 지우기(clf).
 plot(1:M,pp);grid;hold on; % x= [1, M], y= 각 pp값
 plot([1 M],[pi pi],'r','LineWidth',2); % x 구간 [1, M]에 대해 직선 y=pi 긋는다.
-xlabel('Number of Random Points (*10^2))');
+xlabel('Number of Random Points (*10^4))');
 ylabel('Estimation of \pi');
 
 figure(2);clf;
@@ -49,3 +38,39 @@ pbaspect([1 1 1]);  % 박스 비율 x:y:z = 1:1:1
 
 format long
 mean(pp(1500:2000))
+
+
+% 타원 그리기. x^2 + 25y^2 = 1
+% 직사각형 면적=0.4*2=0.8, 직사각형 내부의 점=N.
+% 타원의 면적=0.2pi, 타원 내부의 점=c=pi*N/4.
+% pi=c*4/N
+% 타원의 면적 추정=0.8 * c / N
+
+m=0;
+ellipse_area=zeros(1, M);
+for N = k:k:k*M
+    m = m + 1;
+    x = -1 + 2 * rand(1, N); % [-1, 1]
+    y = -0.2 + 0.4 * rand(1, N); % [-0.2, 0.2]
+    z = sqrt(x.^2 + 25 * y.^2);
+    
+    c = sum(z <= 1);
+    d = N - c;
+
+    ellipse_area(m)=0.8 * c / N;
+end
+
+figure(3);clf;
+plot(1:M,ellipse_area);grid;hold on;
+plot([1 M],[0.2*pi 0.2*pi],'r','LineWidth',2);
+xlabel('Number of Random Points (*10^4))');
+ylabel('Estimation of the area of the ellipse');
+
+figure(4);clf;
+plot(x,y,'.'); hold on; grid;
+xx=-1:0.001:1;
+plot(xx, sqrt((1-xx.^2)/25), 'r', 'LineWidth', 2); hold on; grid;
+plot(xx, -sqrt((1-xx.^2)/25), 'r', 'LineWidth', 2);
+
+format long
+mean(ellipse_area(1500:2000))
