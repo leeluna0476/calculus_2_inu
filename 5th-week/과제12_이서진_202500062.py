93c108114b6c7fa38e5efed2586b6cf05da498a9
LEE LUNA <seojilee@student.42seoul.kr>
2025-10-07 20:23:34 +0900

    5th week attch

---
+++ 5th-week/과제12_이서진_202500062.py
@@ -0,0 +1,24 @@
+import math
+
+SQRT_2PI_INV = 1 / math.sqrt(2 * math.pi)
+
+def std_normalized_gaussian_pdf(z: float):
+    return SQRT_2PI_INV * math.pow(math.e, -(z * z / 2))
+
+def std_normalized_gaussian_cdf(z: float):
+    ret = 0
+    du = 0.00001
+    i = -10.0
+    while i < z:
+        ret += std_normalized_gaussian_pdf(i) * du
+        i += du
+    return ret
+
+print('\t|0.00\t|0.01\t|0.02\t|0.03\t|0.04\t|0.05\t|0.06\t|0.07\t|0.08\t|0.09\t|')
+for i in range(-35, 35):
+    print(f'|{i/10:.2f}\t|', end='')
+    for j in range(10):
+        z = i / 10 + j / 100
+        result = std_normalized_gaussian_cdf(z)
+        print(f'{result:.4f}\t|', end='')
+    print()
